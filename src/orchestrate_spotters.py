7944f9e17964f3301385165a493cae307086d29a
patrick borowy <patrick.borowy@protonmail.com>
2024-03-23 13:26:05 +0100

    dev: spotter orchestration gives orchesator container name to spotters

--- src/orchestrate_spotters.py
+++ src/orchestrate_spotters.py
@@ -7,62 +7,83 @@ from get_spot_ponderation import get_ponderation
 orchestration_label = "network.exorde.orchestrate"
 monitoring_label = "network.exorde.monitor"
 
+async def get_self_container(client):
+    containers = await client.containers.list(
+        filters={"label": ["network.exorde.monitor=true"]}
+    )
+    # this assumes the correct label and that this is not happening while
+    # in an update process 
+    # ( where multiple containers might correctly match this label )
+    return containers[0]
 
-def build_container_conciliator():
-    image_prefix = "exordelabs"  # Prefix to convert module names to image names
-    async def reconcile_containers(desired_state):
-        client = Docker()
-        logging.info("reconcile loop")
-        
-        # Fetch all managed containers
-        containers = await client.containers.list(
-            filters={"label": ["network.exorde.orchestrate=spotter"]}
-        )
 
-        logging.info(f"desired_state: {desired_state}")
-        logging.info(f"found {len(containers)} containers")
-        
-        # Organize containers by their image prefix
-        current_state = {}
-        for container in containers:
-            container_details = await container.show()
-            image_name = container_details['Config']['Image']
-            base_image_name = image_name.replace(f"{image_prefix}/spot", "")
-            
-            if base_image_name in current_state:
-                current_state[base_image_name].append(container)
-            else:
-                current_state[base_image_name] = [container]
-        
-        # Reconcile containers
-        for image, desired_count in desired_state.items():
-            prefixed_image = f"{image_prefix}/spot{image}"  # Apply prefix to image name
-            current_containers = current_state.get(image, [])
-            current_count = len(current_containers)
-            
-            if current_count < desired_count:
-                # Start new containers
-                for _ in range(desired_count - current_count):
-                    logging.info(f"Starting new container for image {prefixed_image}...")
-                    container = await client.containers.create_or_replace(
-                        config={"Image": prefixed_image, "Labels": {
+image_prefix = "exordelabs"  # Prefix to convert module names to image names
+async def reconcile_containers(desired_state):
+    """
+    Iterate over desired_state and makes sure that containers are properly
+    active for each image
+        ( desired_state = { image: amount_of_container } )
+    """
+    client = Docker()
+    logging.info("reconcile loop")
+    
+    # Fetch all managed containers
+    containers = await client.containers.list(
+        filters={"label": ["network.exorde.orchestrate=spotter"]}
+    )
+
+    logging.info(f"desired_state: {desired_state}")
+    logging.info(f"found {len(containers)} spot containers")
+    
+    # Organize containers by their image prefix
+    current_state = {}
+    for container in containers:
+        container_details = await container.show()
+        image_name = container_details['Config']['Image']
+        base_image_name = image_name.replace(f"{image_prefix}/spot", "")
+
+        if base_image_name in current_state:
+            current_state[base_image_name].append(container)
+        else:
+            current_state[base_image_name] = [container]
+
+    self_container = await get_self_container(client)
+    self_container_details = await self_container.show()
+    # Reconcile containers
+    for image, desired_count in desired_state.items():
+        prefixed_image = f"{image_prefix}/spot{image}"
+        current_containers = current_state.get(image, [])
+        current_count = len(current_containers)
+
+        if current_count < desired_count:
+            # Start new containers
+            for _ in range(desired_count - current_count):
+                logging.info(
+                    f"Starting new container for image {prefixed_image}..."
+                )
+                container = await client.containers.create_or_replace(
+                    config={
+                        "Image": prefixed_image, 
+                        "Labels": {
                             "network.exorde.orchestrate": "spotter",
                             "network.exorde.monitor": "true"
-                        }},
-                        name=f"{image_prefix}_{image}_{current_count + _}"
-                    )
-                    await container.start()
-            elif current_count > desired_count:
-                # Stop and remove extra containers
-                extra_containers = current_containers[desired_count:]
-                for container in extra_containers:
-                    logging.info(f"Stopping and removing container {container.id} for image {prefixed_image}...")
-                    await container.stop()
-                    await container.delete()
+                        },
+                        "Env": [
+                            f"ORCHESTRATOR_NAME={self_container_details['Name']}"
+                        ]
+                    },
+                    name=f"{image_prefix}_{image}_{current_count + _}"
+                )
+                await container.start()
+        elif current_count > desired_count:
+            # Stop and remove extra containers
+            extra_containers = current_containers[desired_count:]
+            for container in extra_containers:
+                logging.info(f"Stopping and removing container {container.id} for image {prefixed_image}...")
+                await container.stop()
+                await container.delete()
 
-        await client.close()
-    return reconcile_containers
-reconcile_containers = build_container_conciliator()
+    await client.close()
 
 
 async def get_desired_state() -> dict[str, int]:
